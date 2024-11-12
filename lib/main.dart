51c674248ad6626b43930b5303ecde72830ae1de
henrixe <henrixe80@gmail.com>
2024-11-12 10:46:55 -0400

    Missão Card de Personagem -> Concluída

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,120 +8,239 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Alura Quest',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 183, 62, 58)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'AluraQuest'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red.shade500,
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: ListView(
+        children: [
+          PersonagemCard(
+              "Aragorn",
+              "https://th.bing.com/th/id/R.0d96ce0237f2b3c05225f75a2d50dff5?rik=PQpdhbN03x1wuA&pid=ImgRaw&r=0",
+              "Humano",
+              3),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () => {}),
+    );
   }
+}
+
+class PersonagemCard extends StatefulWidget {
+  final String nome;
+  final String url;
+  final String raca;
+  final int forca;
+
+  const PersonagemCard(this.nome, this.url, this.raca, this.forca, {Key? key})
+      : super(key: key);
+
+  @override
+  _PersonagemCardState createState() => _PersonagemCardState();
+}
+
+class _PersonagemCardState extends State<PersonagemCard> {
+  int vida = 100;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Padding(
+      padding: const EdgeInsets.all(8.8),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.red.shade100,
+                borderRadius: BorderRadius.circular(4)),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.red.shade100,
+                            borderRadius: BorderRadius.circular(4)),
+                        width: 100,
+                        height: 120,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(4),
+                            child: Image.network(
+                              widget.url,
+                              fit: BoxFit.cover,
+                            ))),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 5, left: 10),
+                          child: Container(
+                              width: 350,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    widget.nome,
+                                    style: TextStyle(
+                                        fontSize: 24, color: Colors.black),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          if (vida >= 100) {
+                                            vida = 100;
+                                          } else {
+                                            vida++;
+                                          }
+                                        });
+                                      },
+                                      child: Icon(Icons.arrow_drop_up)),
+                                ],
+                              )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4, left: 10),
+                          child: Container(
+                            width: 350,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    widget.raca,
+                                    style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.red.shade900),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          if (vida <= 0) {
+                                            vida = 0;
+                                          } else {
+                                            vida--;
+                                          }
+                                        });
+                                      },
+                                      child: Icon(Icons.arrow_drop_down))
+                                ]),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4, left: 10),
+                          child: Container(
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: widget.forca >= 1
+                                      ? Colors.red.shade900
+                                      : Colors.red[300],
+                                  size: 15,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: widget.forca >= 2
+                                      ? Colors.red.shade900
+                                      : Colors.red[300],
+                                  size: 15,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: widget.forca >= 3
+                                      ? Colors.red.shade900
+                                      : Colors.red[300],
+                                  size: 15,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: widget.forca >= 4
+                                      ? Colors.red.shade900
+                                      : Colors.red[300],
+                                  size: 15,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: widget.forca >= 5
+                                      ? Colors.red.shade900
+                                      : Colors.red[300],
+                                  size: 15,
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.red.shade200,
+                        borderRadius: BorderRadius.circular(4)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Container(
+                            child: LinearProgressIndicator(
+                              value: vida / 100,
+                              color: Colors.yellow.shade500,
+                              backgroundColor: Colors.white,
+                            ),
+                            width: 300,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Text(
+                            'Vida: $vida',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
