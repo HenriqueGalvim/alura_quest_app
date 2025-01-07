533ce67c25362a706779365bf5e378ae810c5193
henrixe <henrixe80@gmail.com>
2025-01-07 11:55:25 -0400

    Missão Interceptadores e Logger -> Concluído

--- lib/service/http_interceptors.dart
+++ lib/service/http_interceptors.dart
@@ -1,3 +1,30 @@
-class HttpInterceptors{
-  
+import 'dart:developer';
+
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:logger/logger.dart';
+
+class HttpInterceptors extends InterceptorContract {
+  Logger logger = Logger();
+  @override
+  Future<BaseRequest> interceptRequest({
+    required BaseRequest request,
+  }) async {
+    logger.d(
+        "Requisição para ${request.url}\nCabeçalhos: ${request.headers}");
+    return request;
+  }
+
+  @override
+  Future<BaseResponse> interceptResponse({
+    required BaseResponse response,
+  }) async {
+    logger.d('----- Response -----');
+    logger.d('Code: ${response.statusCode}');
+    if (response is Response) {
+      log((response).body);
+          logger.d(
+        "Cabeçalhos: ${response.headers}\nCorpo: ${response.body}");
+    }
+    return response;
+  }
 }
--- lib/service/personagem_service.dart
+++ lib/service/personagem_service.dart
@@ -1,16 +1,20 @@
 import 'package:alura_quest_app/models/personagem.dart';
+import 'package:alura_quest_app/service/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http_interceptor.dart';
 
 class PersonagemService {
   static const String urlBase = "http://172.25.128.1:3000/";
   static const String subDomain = "personagem/";
+  http.Client client =
+      InterceptedClient.build(interceptors: [HttpInterceptors()]);
 
   String geturl() {
     return "$urlBase$subDomain";
   }
 
   create(Personagem personagem) {
-    http.post(Uri.parse(geturl()), body: {
+    client.post(Uri.parse(geturl()), body: {
       "personagem": {
         "nome": personagem.nome,
         "url": personagem.url,
@@ -22,7 +26,7 @@ class PersonagemService {
   }
 
   get() async {
-    http.Response response = await http.get(Uri.parse(geturl()));
+    http.Response response = await client.get(Uri.parse(geturl()));
     return response.body;
   }
 }
